674bf0df5886bdc1d77afe5d81e8e8c58b42c82d
Qim <101701523+qianmo8@users.noreply.github.com>
2023-08-18 07:14:25 +0800

    Update wxyd.py

--- wxyd.py
+++ wxyd.py
@@ -1,6 +1,6 @@
 """
 @Qim出品 仅供学习交流，请在下载后的24小时内完全删除 请勿将任何内容用于商业或非法目的，否则后果自负。
-微信阅读_V1.2   入口：https://postimg.cc/YGQHNhVp
+微信阅读_V1.2   入口：https://i.postimg.cc/Xq75gzft/aab768d55c2e1dfdf368aff59409a3c.jpg
 阅读文章抓出cookie，time，sign 建议手动阅读5篇左右再使用脚本，不然100%黑！！！一小时一次，每天到底几轮自己测试
 export ydtoken=cookie
 多账号用'===='隔开 例 账号1====账号2

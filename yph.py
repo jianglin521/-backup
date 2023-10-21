94a696b1ac36af9b8d3b208c0b39c7fdcc784a3c
Qim <101701523+qianmo8@users.noreply.github.com>
2023-10-21 13:46:58 +0800

    Update yph.py

--- yph.py
+++ yph.py
@@ -1,23 +1,17 @@
 """
 @Qim出品 仅供学习交流，请在下载后的24小时内完全删除 请勿将任何内容用于商业或非法目的，否则后果自负。
-玉品汇_V0.1   红包
+玉品汇_V0.11   红包
 入口 http://up.yuojo.com/index/index/register?code=61DM98
 抓包域名 http://up.yuojo.com取出token
 export yphtoken=token
 多账号用'===='隔开 例 账号1====账号2
 cron： 0 0 7,21 * * ?
 """
+xianshi_dh = 1  # 1开 0关   仙石兑换开关
+yushi_cs = 1  # 1开 0关   红包兑换开关
 
-
-
-
-
-
-
-
-
-import time
 import os
+import time
 import requests
 
 accounts = os.getenv('yphtoken')
@@ -59,23 +53,26 @@ else:
             url = 'http://up.yuojo.com/api/user/getSignProfit'
             response = requests.post(url, headers=headers, json=data).json()
             msg = response['msg']
-            print(f"==============仙石兑换==============")
-            url = "http://up.yuojo.com/api/user/info"
-            response = requests.post(url, headers=headers).json()
-            shuijing = response['data']['shuijing']
-            shuijing = float(shuijing)
-            num = int(shuijing) // 2
+            if xianshi_dh == 1:
+                print(f"==============仙石兑换==============")
+                url = "http://up.yuojo.com/api/user/info"
+                response = requests.post(url, headers=headers).json()
+                shuijing = response['data']['shuijing']
+                shuijing = float(shuijing)
+                num = int(shuijing) // 2
 
-            if num == 0:
-                print("水晶余额不足,,退出")
-            else:
-                url = "http://up.yuojo.com/api/yupu/shuijingToXianshi"
-                data = {
-                    'num': num
-                }
-                response = requests.post(url, headers=headers, data=data).json()
-                msg = response['msg']
-                print(msg)
+                if num == 0:
+                    print("水晶余额不足,,退出")
+                else:
+                    url = "http://up.yuojo.com/api/yupu/shuijingToXianshi"
+                    data = {
+                        'num': num
+                    }
+                    response = requests.post(url, headers=headers, data=data).json()
+                    msg = response['msg']
+                    print(msg)
+            elif xianshi_dh == 0:
+                print("仙石兑换No")
             print(f"==============领取分红==============")
             for i in range(2):
                 url = "http://up.yuojo.com/api/circle/send_ad"
@@ -100,14 +97,17 @@ else:
                 yushi = response['data']['yushi']
                 money = response['data']['money']
                 print(f"{nickname} 红包余额:{money}\n水晶:{shuijing} 仙石:{xianshi} 玉石:{yushi}")
-                print(f"==============兑换红包==============")
-                url="http://up.yuojo.com/api/yupu/saleYushi"
-                data = {
-                    'sale_num': yushi
-                }
-                response = requests.post(url, headers=headers, json=data).json()
-                msg = response['msg']
-                print(msg)
+                if yushi_cs == 1:
+                    print(f"=============玉石兑换==============")
+                    url = "http://up.yuojo.com/api/yupu/saleYushi"
+                    data = {
+                        'sale_num': yushi
+                    }
+                    response = requests.post(url, headers=headers, json=data).json()
+                    msg = response['msg']
+                    print(msg)
+                elif yushi_cs == 0:
+                    print(f"兑换玉石No")
             else:
                 print(f"错误未知{response}")
         else:
